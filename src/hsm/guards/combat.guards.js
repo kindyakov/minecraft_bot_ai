08b2b8a73d627d5f792f8ffdc6bb466787a4ad55
kindyakov <kindyakov_2016@mail.ru>
2025-09-29 01:16:05 +0300

    _

--- src/hsm/guards/combat.guards.js
+++ src/hsm/guards/combat.guards.js
@@ -14,22 +14,31 @@ const isLowLealth = and([
   ({ context, event }) => context.health <= 8
 ])
 
-const isSurrounded = ({ context, event }) => false
+const isSurrounded = and([
+  not(stateIn({ MAIN_ACTIVITY: { COMBAT: 'DEFENDING' } })),
+  ({ context, event }) => context.health > 8,
+  ({ context, event }) => false
+])
 
 const isEnemyFar = and([
+  not(stateIn({ MAIN_ACTIVITY: { COMBAT: 'RANGED_ATTACKING' } })),
+  ({ context, event }) => context.health > 8,
   ({ context }) => {
     const rangedWeapon = context.bot.utils.getRangeWeapon() // поиск оружия лук/арбалет
     const arrows = context.bot.utils.getArrow()
     return rangedWeapon && arrows
   },
   ({ context: { nearestEnemy }, event }) => {
-    return nearestEnemy && nearestEnemy.distance > 5
+    return nearestEnemy && nearestEnemy.distance > 7
   }
 ])
 
-const isEnemyClose = ({ context: { nearestEnemy }, event }) => {
-  return nearestEnemy && nearestEnemy.distance <= 5
-}
+const isEnemyClose = and([
+  not(stateIn({ MAIN_ACTIVITY: { COMBAT: 'MELEE_ATTACKING' } })),
+  ({ context, event }) => context.health > 8,
+  ({ context: { nearestEnemy }, event }) => {
+    return nearestEnemy && nearestEnemy.distance <= 7
+  }])
 
 export default {
   noEnemiesNearby,
